ee4a1fc7b2c81ad30eec06a34045658b3facf58f
vladislav t <vt118@mail.ru>
2020-03-06 00:33:27 +0300

    feat: finish function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let configArr = bracketsConfig.map(item => item.join(''));
+  let length;
+  do {
+      length = str.length;
+      for (let i = 0; i < configArr.length; i++) {
+          str = str.replace(configArr[i], '');
+          console.log(str);
+      }
+  } while (length !== str.length);
+
+  return !str.length;
+};
